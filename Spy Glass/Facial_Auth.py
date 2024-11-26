14916bc2f034f8754591b1e2ad177d7aa7661e93
HYOCC <96851545+HYOCC@users.noreply.github.com>
2024-11-25 22:05:44 -0500

    Add files via upload

--- Spy Glass/Facial_Auth.py
+++ Spy Glass/Facial_Auth.py
@@ -1,7 +1,7 @@
 import os
-import face_recognition
+from face_recognition import load_image_file, face_locations, face_encodings, compare_faces
 import cv2 
-import time 
+from time import sleep
 
 def first_time_auth(user_id:str, Name:str):
     cam = cv2.VideoCapture(0)
@@ -40,7 +40,7 @@ def first_time_auth(user_id:str, Name:str):
                 
                 
                 # Detect face locations in the frame
-                face_location = face_recognition.face_locations(rgb_frame, model="hog")  # Use "cnn" for GPU acceleration
+                face_location = face_locations(rgb_frame, model="hog")  # Use "cnn" for GPU acceleration
                 
                 if face_location:
                     # Select only the first detected face
@@ -55,7 +55,7 @@ def first_time_auth(user_id:str, Name:str):
                     
             slow_down_pic_frame += 1
             #Adds a delay for the next iteration
-            time.sleep(0.15)
+            sleep(0.15)
             
         run  = False 
     
@@ -66,17 +66,61 @@ def first_time_auth(user_id:str, Name:str):
 
 
 
+def authenticate(user_id:str):
+    #Loads in available images from given user id to load in data
+    if os.path.exists(f'Known_Face/{user_id}'):
+        user_id_dir = f'Known_Face/{user_id}'            
+        user_id_with_face = {}
 
-user_id = input("Choose your unique ID: ")
-user_id_prefer_name = input('What would you like to be called?: ')
-first_time_auth(user_id, user_id_prefer_name)
+        for image_name in os.listdir(user_id_dir):
+            image_path_file = os.path.join(user_id_dir, image_name)
+            print(image_path_file)#Logging
+            
+            image = load_image_file(image_path_file)
+            encodings = face_encodings(image)
+
+            if encodings:
+                if user_id_with_face:
+                    user_id_with_face[user_id].append(encodings[0])
+                else:
+                    user_id_with_face[user_id] = [encodings[0]]
+                #known_names.append(os.path.splitext(image_name)[0])  # Use filename as label
+            print('known faces lodaded and encoded')
+            print(user_id_with_face)
+    else:
+        print('Cant find user_id ' + user_id)
+        print(f'Known_Face/{user_id}')
+        return 
     
+    run = True 
+    #Starts cam
+    cam = cv2.VideoCapture(0)
+    #Starts authenticating User
+    while run:
+        ret, frame = cam.read() #Checks if frames was successfully captured then save the frame onto 'frame'
+    
+        if not ret:
+            print('failed to grab ret')
+            break
         
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) #Changes the BGR to RGB which is required for facial recognization
         
+        face_location = face_locations(rgb_frame, model='hog') #Detects faces 
+        face_encoding = face_encodings(rgb_frame, face_location) #Encodes the faces 
         
+        #Compares the seen face to known faces
+        for (top, right, bottom, left), encoding in zip(face_location, face_encoding):
+            matches = compare_faces(user_id_with_face[user_id], encoding)
+            if True in matches:
+                print('detected ' + user_id)
+                run = False #breaks the finding person function once a detected person is found
+    #Lets go all of resouces such as the cam etc
+    cam.release()
+    cv2.destroyAllWindows()
         
-        
-        
-         
     
     
+    
+
+    
+authenticate('HYO)C')
